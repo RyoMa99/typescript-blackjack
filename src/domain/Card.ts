14395b17bb667c89547dc257e9e33a62e491888b
RyoMa99 <hatanakaryoma.130@gmail.com>
2021-07-18 22:10:23 +0900

    cardのnumberをnumberで保有。get時に数値型とstring型で取り出せるように修正。

--- src/domain/Card.ts
+++ src/domain/Card.ts
@@ -2,20 +2,35 @@ import CardNumber from "../values/CardNumber";
 import Mark from "../values/Mark";
 
 class Card {
-  private readonly number: CardNumber;
-  private readonly mark: Mark;
+  private readonly _number: CardNumber;
+  private readonly _mark: Mark;
 
-  constructor(number: string,mark: string){
-    this.number = CardNumber.of(number);
-    this.mark = Mark.of(mark);
+  constructor(number: number, mark: string) {
+    this._number = CardNumber.of(number);
+    this._mark = Mark.of(mark);
   }
 
-  getNumber(): CardNumber{
-    return this.number;
+  getNumber(): number {
+    return this._number.get();
   }
 
-  getMark(): Mark{
-    return this.mark;
+  getNumberString(): string {
+    switch (this._number.get()) {
+      case 1:
+        return "A";
+      case 11:
+        return "J";
+      case 12:
+        return "Q";
+      case 13:
+        return "K";
+      default:
+        return this._number.get().toString();
+    }
+  }
+
+  getMark(): string {
+    return this._mark.get();
   }
 }
 
--- src/values/CardNumber.ts
+++ src/values/CardNumber.ts
@@ -1,13 +1,12 @@
 import ValueObject from "./base";
 
-class CardNumber extends ValueObject<string> {
-  
-  isValid(val: string):boolean{
-    const permissableNumber = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+class CardNumber extends ValueObject<number> {
+  isValid(val: number): boolean {
+    const permissableNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     return permissableNumber.includes(val);
   }
 
-  static of(_val: string): CardNumber {
+  static of(_val: number): CardNumber {
     return new this(_val);
   }
 }
